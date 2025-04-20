37ea9862cf59f75892df2d0727706312a063d7bc
SANDEEP KUMAR <sndpmndwl@gmail.com>
2025-04-20 11:58:07 +0530

    Create types.ts
    Create types.ts

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+export  interface Worker {
+  id: string;
+  name: string;
+  profession: string;
+  location: string;
+  rate: string;
+  phone: string;
+  image: string;
+  rating: number;
+  description: string;
+}
+ 
